c99d144e04504579d487eb9a40add061f30ca2d6
Lemintmint <lemgm1237@gmail.com>
2024-04-27 21:03:29 +0900

    2024/04/26

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
@@ -22,13 +29,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
                 }
             }
         }
     }
 }
 
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -36,11 +44,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+*/
+
+@Composable
+fun DisplayComposable(): Unit {
+    Box(
+        modifier = Modifier
+            .background(color = colorResource(id = R.color.purple1))
+            .align(Alignment.Top)
+            .weight(1f)
+    ) {
+
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Row(
+
+        ){
+
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        DisplayComposable()
     }
 }
