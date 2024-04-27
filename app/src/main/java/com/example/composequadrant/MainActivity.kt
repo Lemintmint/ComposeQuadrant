e6dc23408050c6d147ef6cdc384e9b62863a8e16
Lemintmint <lemgm1237@gmail.com>
2024-04-27 22:30:36 +0900

    2024/04/27

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.composequadrant
 
 import android.os.Bundle
+import android.support.v4.os.IResultReceiver2.Default
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
-import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,7 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,41 +34,129 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting("Android")
+                    DisplayComposable()
                 }
             }
         }
     }
 }
 
-/*
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-*/
-
 @Composable
 fun DisplayComposable(): Unit {
-    Box(
-        modifier = Modifier
-            .background(color = colorResource(id = R.color.purple1))
-            .align(Alignment.Top)
-            .weight(1f)
-    ) {
-
-    }
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(color = colorResource(id = R.color.purple1))
     ) {
         Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(color = colorResource(id = R.color.purple1))
+                    .align(Alignment.Top)
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.text_title),
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.text_sentence),
+                        textAlign = TextAlign.Justify
+                    )
+                }
 
-        ){
+            }
 
+            Box(
+                modifier = Modifier
+                    .background(color = colorResource(id = R.color.purple2))
+                    .align(Alignment.Top)
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.image_title),
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.image_sentence),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(color = colorResource(id = R.color.purple3))
+                    .align(Alignment.Top)
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.row_title),
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.row_sentence),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .background(color = colorResource(id = R.color.purple4))
+                    .align(Alignment.Top)
+                    .weight(1f)
+                    .fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.column_title),
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.column_sentence),
+                        textAlign = TextAlign.Justify
+                    )
+                }
+            }
         }
     }
 }
